d7dcc143a43d50298b74e1bd12a24f4e397749ba
nswartzentruber <nswartzentruber@gmail.com>
2018-02-27 20:38:15 -0500

    Begin removing unnecessary sample functionality

--- Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.kt
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.kt
@@ -19,30 +19,20 @@ package com.example.android.bluetoothlegatt
 import android.app.Activity
 import android.app.ListActivity
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ListView
-import android.widget.TextView
 import android.widget.Toast
-
-import java.util.ArrayList
 
 /**
  * Activity for scanning and displaying available Bluetooth LE devices.
  */
 class DeviceScanActivity : ListActivity() {
-    private var mLeDeviceListAdapter: LeDeviceListAdapter? = null
     private var mBluetoothAdapter: BluetoothAdapter? = null
     private var mScanning: Boolean = false
     private var mHandler: Handler? = null
@@ -52,8 +42,14 @@ class DeviceScanActivity : ListActivity() {
         runOnUiThread {
             // Only connect to LEGO Move Hubs
             if (device != null && device.name != null && device.name == "LEGO Move Hub") {
-                mLeDeviceListAdapter!!.addDevice(device)
-                mLeDeviceListAdapter!!.notifyDataSetChanged()
+                val intent = Intent(this, DeviceControlActivity::class.java)
+                intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.name)
+                intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.address)
+                if (mScanning) {
+                    mBluetoothAdapter!!.stopLeScan(null)
+                    mScanning = false
+                }
+                startActivity(intent)
             }
         }
     }
@@ -101,7 +97,6 @@ class DeviceScanActivity : ListActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu_scan -> {
-                mLeDeviceListAdapter!!.clear()
                 scanLeDevice(true)
             }
             R.id.menu_stop -> scanLeDevice(false)
@@ -121,9 +116,6 @@ class DeviceScanActivity : ListActivity() {
             }
         }
 
-        // Initializes list view adapter.
-        mLeDeviceListAdapter = LeDeviceListAdapter()
-        listAdapter = mLeDeviceListAdapter
         scanLeDevice(true)
     }
 
@@ -139,19 +131,6 @@ class DeviceScanActivity : ListActivity() {
     override fun onPause() {
         super.onPause()
         scanLeDevice(false)
-        mLeDeviceListAdapter!!.clear()
-    }
-
-    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
-        val device = mLeDeviceListAdapter!!.getDevice(position) ?: return
-        val intent = Intent(this, DeviceControlActivity::class.java)
-        intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.name)
-        intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.address)
-        if (mScanning) {
-            mBluetoothAdapter!!.stopLeScan(mLeScanCallback)
-            mScanning = false
-        }
-        startActivity(intent)
     }
 
     private fun scanLeDevice(enable: Boolean) {
@@ -170,68 +149,6 @@ class DeviceScanActivity : ListActivity() {
             mBluetoothAdapter!!.stopLeScan(mLeScanCallback)
         }
         invalidateOptionsMenu()
-    }
-
-    // Adapter for holding devices found through scanning.
-    private inner class LeDeviceListAdapter : BaseAdapter() {
-        private val mLeDevices: ArrayList<BluetoothDevice> = ArrayList()
-        private val mInflator: LayoutInflater = this@DeviceScanActivity.layoutInflater
-
-        fun addDevice(device: BluetoothDevice?) {
-            if (!mLeDevices.contains(device)) {
-                mLeDevices.add(device!!)
-            }
-        }
-
-        fun getDevice(position: Int): BluetoothDevice? {
-            return mLeDevices[position]
-        }
-
-        fun clear() {
-            mLeDevices.clear()
-        }
-
-        override fun getCount(): Int {
-            return mLeDevices.size
-        }
-
-        override fun getItem(i: Int): Any {
-            return mLeDevices[i]
-        }
-
-        override fun getItemId(i: Int): Long {
-            return i.toLong()
-        }
-
-        override fun getView(i: Int, viewInput: View?, viewGroup: ViewGroup): View {
-            var view = viewInput
-            val viewHolder: ViewHolder
-            // General ListView optimization code.
-            if (view == null) {
-                view = mInflator.inflate(R.layout.listitem_device, null)
-                viewHolder = ViewHolder()
-                viewHolder.deviceAddress = view!!.findViewById<View>(R.id.device_address) as TextView
-                viewHolder.deviceName = view.findViewById<View>(R.id.device_name) as TextView
-                view.tag = viewHolder
-            } else {
-                viewHolder = view.tag as ViewHolder
-            }
-
-            val device = mLeDevices[i]
-            val deviceName = device.name
-            if (deviceName != null && deviceName.length > 0)
-                viewHolder.deviceName!!.text = deviceName
-            else
-                viewHolder.deviceName!!.setText(R.string.unknown_device)
-            viewHolder.deviceAddress!!.text = device.address
-
-            return view
-        }
-    }
-
-    internal class ViewHolder {
-        var deviceName: TextView? = null
-        var deviceAddress: TextView? = null
     }
 
     companion object {
